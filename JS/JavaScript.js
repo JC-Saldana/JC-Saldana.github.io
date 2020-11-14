9e6ff19f1d4ffdfceeb0efbf11be3aa436a14449
Carlos Saldaña <jcarlossaldae@gmail.com>
2020-11-14 01:51:19 +0100

    translate feature added

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,4 +1,14 @@
- 
-function english() {
-  document.getElementById("icons").innerHTML = "YOU CLICKED ME!";
+
+window.addEventListener('load', function() { 
+  document.getElementById("english").addEventListener("click", inglés) 
+});
+
+function inglés () {
+  document.getElementById("txtIndex").innerHTML = txtInglés[0];
+  document.getElementById("txtPizza").innerHTML = txtInglés[1];
 }
+
+var txtInglés = [
+  "Prueba español 1", // Texto index
+  "Prueba español 2"  //Indefinido
+]
