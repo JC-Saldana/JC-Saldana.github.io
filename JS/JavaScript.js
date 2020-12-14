39233550e705e2a293db3047b7387460981e71cc
Carlos Saldaña <jcarlossaldae@gmail.com>
2020-12-14 01:37:48 +0100

    now using JQuery

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,9 +1,13 @@
-// Idioma
 
-window.addEventListener('load', function() { 
+// Al cargar
+
+$(inicio)
+function inicio() {
   idiomaInicial()
   document.getElementById("idiomas").addEventListener("click", cambiarIdioma)  
-});
+}
+
+// Idiomas
 
 function idiomaInicial() {
   if (localStorage.lastname == "ing") {
@@ -20,18 +24,16 @@ function cambiarIdioma() {
   }
 }
 
-//$("#div3").html("<h3><font color='lightgreen'>A disfrutar con el Javascript</font></h3>")
-//Quizás usar append
 function español () {
   localStorage.setItem("lastname", "ing")
-  document.getElementById("txtIndex").innerHTML = txtEspañol[0];
-  document.getElementById("sureButton").innerHTML = txtEspañol[1];
+  $("#txtIndex").html(txtEspañol[0])
+  $("#sureButton").html(txtEspañol[1])
 }
 
 function inglés () {
   localStorage.setItem("lastname", "esp")
-  document.getElementById("txtIndex").innerHTML = txtInglés[0];
-  document.getElementById("sureButton").innerHTML = txtInglés[1];
+  $("#txtIndex").html(txtInglés[0])
+  $("#sureButton").html(txtInglés[1])
 }
 
 var txtInglés = [
